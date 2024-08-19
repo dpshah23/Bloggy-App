cb05d9e9bc36a3405ce21926ffbea78b537e85e2
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-08-19 15:42:54 +0530

    updated ui

--- app/src/main/java/com/example/bloggy/ForgetPassword.java
+++ app/src/main/java/com/example/bloggy/ForgetPassword.java
@@ -3,12 +3,31 @@ package com.example.bloggy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import  android.widget.Button;
 
 public class ForgetPassword extends AppCompatActivity {
+
+
+    EditText email;
+    Button forgetbtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+
+        email=findViewById(R.id.emailInput);
+        forgetbtn=findViewById(R.id.forgetpasswordbtnsubmit);
+
+        forgetbtn.setOnClickListener(v -> {
+            String emailinp=email.getText().toString();
+
+            System.out.println(emailinp);
+
+
+
+        });
     }
 }
