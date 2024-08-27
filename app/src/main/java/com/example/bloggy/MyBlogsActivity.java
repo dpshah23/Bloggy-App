2870816b4baa7f88d19801262df32dd535929ccf
RaghavRaval <raghavshayar3507@gmail.com>
2024-08-27 18:59:41 +0530

    my blogs done

--- app/src/main/java/com/example/bloggy/MyBlogsActivity.java
+++ app/src/main/java/com/example/bloggy/MyBlogsActivity.java
@@ -10,10 +10,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MyBlogsActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private BlogAdapter blogAdapter;
+    private List<Blog> blogList = new ArrayList<>();
+    private boolean isLoading = false;
+    private int pageNumber = 1;
+    private int totalPages = 1;  // Assuming the API returns this value
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +45,35 @@ public class MyBlogsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_blogs);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnav);
         bottomNavigationView.setSelectedItemId(R.id.myblog);
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        blogAdapter = new BlogAdapter(blogList, this);
+        recyclerView.setAdapter(blogAdapter);
+
+        // Fetch the initial set of blogs
+        fetchBlogsFromApi(pageNumber);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                int visibleItemCount = layoutManager.getChildCount();
+                int totalItemCount = layoutManager.getItemCount();
+                int pastVisibleItems = layoutManager.findFirstVisibleItemPosition();
+
+                if (!isLoading && pageNumber < totalPages) {
+                    if ((visibleItemCount + pastVisibleItems) >= totalItemCount * 0.7) {
+                        pageNumber++;
+                        fetchBlogsFromApi(pageNumber);
+                    }
+                }
+            }
+        });
+
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -51,4 +103,65 @@ public class MyBlogsActivity extends AppCompatActivity {
             return insets;
         });
     }
-}
+        private void fetchBlogsFromApi(int page) {
+            isLoading = true;
+            OkHttpClient client = new OkHttpClient();
+            String url = "http://10.0.2.2:8000/api/getuserblog/dpshah2307/" + page; // Assuming the API supports pagination
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    e.printStackTrace();
+                    isLoading = false;
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        String responseData = response.body().string();
+                        try {
+                            JSONObject jsonResponse = new JSONObject(responseData);
+                            JSONArray blogsArray = jsonResponse.getJSONArray("data");
+                            totalPages = jsonResponse.getInt("total_pages");  // Get the total pages from the API response
+
+                            for (int i = 0; i < blogsArray.length(); i++) {
+                                JSONObject blogObject = blogsArray.getJSONObject(i);
+
+                                Blog blog = new Blog(
+                                        blogObject.getString("content"),
+                                        blogObject.getString("email"),
+                                        blogObject.getString("id"),
+                                        blogObject.getString("image"),
+                                        blogObject.getString("timestamp"),
+                                        blogObject.getString("title"),
+                                        blogObject.getString("username")
+                                );
+
+                                blogList.add(blog);
+                            }
+
+                            // Update UI on the main thread
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    blogAdapter.notifyDataSetChanged();
+                                }
+                            });
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } finally {
+                            isLoading = false;
+                        }
+                    } else {
+                        isLoading = false;
+                    }
+                }
+            });
+
+        }
+    }
