3aca637d57d728e31c217b446474b3741cbf2fc2
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-08-28 11:04:36 +0530

    added refresh

--- app/src/main/java/com/example/bloggy/NoInternetActivity.java
+++ app/src/main/java/com/example/bloggy/NoInternetActivity.java
@@ -1,6 +1,8 @@
 package com.example.bloggy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class NoInternetActivity extends AppCompatActivity {
 
+    ImageView refreshimg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,15 @@ public class NoInternetActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        refreshimg=findViewById(R.id.refresh);
+
+        refreshimg.setOnClickListener(v -> {
+            Intent intent=new Intent(NoInternetActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+
         });
     }
 }
